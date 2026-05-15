0ed4555ef095609999a90a76929dd0836a319e91
DIWAKAR <didk9675@gmail.com>
2026-05-15 11:58:41 +0530

    fix import

--- src/components/images/graphics/designPath.js
+++ src/components/images/graphics/designPath.js
@@ -17,10 +17,11 @@ import rjB8  from "./rjB8.png" ;
 import rjB9  from "./rjB9.png" ;
 import rjB10 from "./rjB10.png" ;
 import rjB11 from "./rjB11.png" ;
+import cshare from "./cshare.png" ;
 
 
 
 
 
 
-export {   rjB1, rjB2, rjB3, rjB4, rjB5, rjB6, rjB7, rjB8, rjB9, rjB10, rjB11, bBanner, bDesign, BLogo, hostinger, rjLogo, sBlockElements, Figma, }
+export {   rjB1, rjB2, rjB3, rjB4, rjB5, rjB6, rjB7, rjB8, rjB9, rjB10, rjB11, bBanner, bDesign, BLogo, hostinger, rjLogo, sBlockElements, Figma, cshare}
